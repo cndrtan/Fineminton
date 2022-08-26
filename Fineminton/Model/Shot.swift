d88c71e9d64505c51198f87d2ecabefe3b266c3e
trdsaputri <88604546+trdsaputri@users.noreply.github.com>
2022-08-26 15:54:07 +0700

    Feat: Initial Commit
    create front-end for shot instruction

---
+++ Fineminton/Model/Shot.swift
@@ -0,0 +1,20 @@
+//
+//  Shot.swift
+//  Fineminton
+//
+//  Created by Theresia Saputri on 26/08/22.
+//
+
+import Foundation
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Shot: Hashable, Codable, Identifiable{
+    var id: Int
+    var shotName: String
+    var shotImage: String
+    var shotSummary: String
+    var shotDescription: String
+    var isOpen: Bool
+}
