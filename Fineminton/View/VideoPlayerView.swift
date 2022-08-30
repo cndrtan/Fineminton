a0365dedd56826996e931c5c3e5172589c98a264
Yoppy Yunhasnawa <yunhasnawa@ymail.com>
2022-08-30 13:23:30 +0700

    feat: add pager for multiple tutorial video

---
+++ Fineminton/View/VideoPlayerView.swift
@@ -0,0 +1,45 @@
+//
+//  VideoPlayerView.swift
+//  Fineminton
+//
+//  Created by Yoppy Yunhasnawa on 30/08/22.
+//
+
+import SwiftUI
+import AVKit
+
+struct VideoPlayerView: View {
+    var body: some View {
+        GeometryReader { geometry in
+            VStack {
+                HStack() {
+                    Text("Pergerakan Pukulan Lob/Clear")
+                    Spacer()
+                }
+                VideoPlayer(player: AVPlayer(url:  URL(string: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")!))
+                    .frame(height: (geometry.size.height * 0.80)).cornerRadius(10)
+                Button(action: {
+                    print("sign up bin tapped")
+                }) {
+                    Text("Mulai Latihan")
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .font(.system(size: 18))
+                        .padding()
+                        .foregroundColor(.white)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.white, lineWidth: 2)
+                        )
+                }
+                .background(Color.blue)
+                .cornerRadius(10)
+            }
+        }
+    }
+}
+
+struct VideoPlayerView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoPlayerView()
+    }
+}
--- Fineminton/View/VideoTutorialView.swift
+++ Fineminton/View/VideoTutorialView.swift
@@ -6,36 +6,18 @@
 //
 
 import SwiftUI
-import AVKit
 
 struct VideoTutorialView: View {
     var body: some View {
         NavigationView {
-            VStack {
-                Spacer()
-                HStack() {
-                    Text("Pergerakan Pukulan Lob/Clear").padding()
-                    Spacer()
-                }
-                VideoPlayer(player: AVPlayer(url:  URL(string: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")!))
-                    .frame(height: 520).cornerRadius(10).padding()
-                Button(action: {
-                    print("sign up bin tapped")
-                }) {
-                    Text("Mulai Latihan")
-                        .frame(minWidth: 0, maxWidth: .infinity)
-                        .font(.system(size: 18))
-                        .padding()
-                        .foregroundColor(.white)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 10)
-                                .stroke(Color.white, lineWidth: 2)
-                        )
-                }
-                .background(Color.blue)
-                .cornerRadius(10)
-                .padding()
+            TabView {
+                VideoPlayerView().padding()
+                VideoPlayerView().padding()
+                VideoPlayerView().padding()
+                VideoPlayerView().padding()
             }
+            .tabViewStyle(.page)
+            .indexViewStyle(.page(backgroundDisplayMode: .always))
             .navigationTitle("Tutorial")
         }
     }
