fafaabf07a9515bdfb167fcf4cce5b3b9c3bbd8e
Yoppy Yunhasnawa <yunhasnawa@ymail.com>
2022-09-06 16:25:41 +0700

    fix: update video and image resolution

--- Fineminton/Model/TutorialModel.swift
+++ Fineminton/Model/TutorialModel.swift
@@ -17,12 +17,12 @@ public class TutorialModel {
     
     public func getStepList() -> Array<TutorialStep> {
         
-        let step1 = TutorialStep(sequenceNumber: 0, title: "Tahap 1", mediaSource: "tahap_1_grip", description: "Genggamlah raket dengan benar (bisa menggunakan \"Handhsake\" atau \"Pistol\" grip).", mediaType: "image")
-        let step2 = TutorialStep(sequenceNumber: 1, title: "Tahap 2", mediaSource: "tahap_2_stance", description: "Lakukanlah \"Waiting\" atau \"Receiving\" stance. \nTahan lengan Anda yang memegang raket dalam posisi pinggiran raket menghadap ke atas. \nAtur keseimbangan Anda pada kedua kaki secara merata.", mediaType: "image")
-        let step3 = TutorialStep(sequenceNumber: 2, title: "Tahap 3", mediaSource: "tahap_3_clear_shot_detail_tangan", description: "Gerakkan pergelangan tangan Anda sampai menghadap ke atas. \nPukul-lah kok, ketika kok berada di arah pukul 11. \nAyunkan raket kedepan setinggi mungkin sampai mencapai kontak dengan kok.")
-        let step4 = TutorialStep(sequenceNumber: 3, title: "Tahap 4", mediaSource: "tahap_4_clear_shot_tutorial_footwork", description: "Kaki kanan maju kedepan mengikuti raket.")
-        let step5 = TutorialStep(sequenceNumber: 4, title: "Tahap 5", mediaSource: "tahap_5_clear_shot", description: "Tarik kembali lengan yang memegang raket ke area belakang, seperti gerakan menarik busur panah.")
-        let step6 = TutorialStep(sequenceNumber: 5, title: "Tahap 6", mediaSource: "tahap_6_clear_shot_fast", description: "Lakukanlah sebanyak 20 kali dengan 5 set.", showActionButton: true)
+        let step1 = TutorialStep(sequenceNumber: 0, title: "Tahap 1", mediaSource: "l0t0", description: "Genggamlah raket dengan benar (bisa menggunakan \"Handhsake\" atau \"Pistol\" grip).", mediaType: "image")
+        let step2 = TutorialStep(sequenceNumber: 1, title: "Tahap 2", mediaSource: "l0t1", description: "Lakukanlah \"Waiting\" atau \"Receiving\" stance. \nTahan lengan Anda yang memegang raket dalam posisi pinggiran raket menghadap ke atas. \nAtur keseimbangan Anda pada kedua kaki secara merata.", mediaType: "image")
+        let step3 = TutorialStep(sequenceNumber: 2, title: "Tahap 3", mediaSource: "l0t2", description: "Gerakkan pergelangan tangan Anda sampai menghadap ke atas. \nPukul-lah kok, ketika kok berada di arah pukul 11. \nAyunkan raket kedepan setinggi mungkin sampai mencapai kontak dengan kok.")
+        let step4 = TutorialStep(sequenceNumber: 3, title: "Tahap 4", mediaSource: "l0t3", description: "Kaki kanan maju kedepan mengikuti raket.")
+        let step5 = TutorialStep(sequenceNumber: 4, title: "Tahap 5", mediaSource: "l0t4", description: "Tarik kembali lengan yang memegang raket ke area belakang, seperti gerakan menarik busur panah.")
+        let step6 = TutorialStep(sequenceNumber: 5, title: "Tahap 6", mediaSource: "l0t5", description: "Lakukanlah sebanyak 20 kali dengan 5 set.", showActionButton: true)
         
         var steps: Array<TutorialStep> = Array()
         steps.append(step1)
